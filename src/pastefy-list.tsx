b8e6158d4a4abdd4f0b702befc8ec1a232586d04
juliangojani <julian@gojani.xyz>
2024-09-17 13:19:56 +0200

    Fixed linting errors

--- src/pastefy-list.tsx
+++ src/pastefy-list.tsx
@@ -1,107 +1,123 @@
 import { Action, ActionPanel, Detail, Icon, List, openExtensionPreferences } from "@raycast/api";
 import { getPreferenceValues } from "@raycast/api";
-import pastefy from './pastefy'
 import { usePromise } from "@raycast/utils";
 import { useState } from "react";
 import axios, { AxiosError } from "axios";
 
-function PastePreview({ paste, onDelete = () => {} }: {paste: any, onDelete: () => void}) {
-    let markdown = `## ${paste.title} ${paste.type === 'MULTI_PASTE' ? '`Multi-Paste`' : ''}\n`;
+type Paste = {
+  id: string;
+  title: string;
+  content: string;
+  type: string;
+  expire_at?: string;
+  encrypted?: boolean;
+  created_at: string;
+};
 
-    const createContentBlock = (title: string, content: string) => {
-        if (paste.title?.endsWith('.md')) {
-            return `---
+function PastePreview({ paste, onDelete = () => {} }: { paste: Paste; onDelete: () => void }) {
+  let markdown = `## ${paste.title} ${paste.type === "MULTI_PASTE" ? "`Multi-Paste`" : ""}\n`;
+
+  const createContentBlock = (title: string, content: string) => {
+    if (paste.title?.endsWith(".md")) {
+      return `---
 ${content}
----`
-        } else {
-            return `\`\`\`${title?.split('.')?.at(-1) || 'js'}
+---`;
+    } else {
+      return `\`\`\`${title?.split(".")?.at(-1) || "js"}
 ${content}
-\`\`\``
-        }
+\`\`\``;
     }
+  };
 
-    if (paste.type === 'MULTI_PASTE') {
-        const parts = JSON.parse(paste.content)
-        for (const {name, contents} of parts) {
-            markdown += `---
+  if (paste.type === "MULTI_PASTE") {
+    const parts = JSON.parse(paste.content);
+    for (const { name, contents } of parts) {
+      markdown += `---
             
 
 ### ${name}
 ${createContentBlock(name, contents)}
-`
-        }
-    } else {
-        markdown += createContentBlock(paste.title, paste.content)
+`;
     }
-
-
-    return <List.Item 
-        icon={Icon.Code}
-        title={paste.title || 'untitled'}
-        detail={
-            <List.Item.Detail markdown={markdown} />
-        }
-        actions={
-            <ActionPanel title="Paste Options">
-                <Action.CopyToClipboard title="Copy Paste Content" content={paste.content} />
-                <Action.Paste title="Paste Content" content={paste.content} />
-                <Action.CopyToClipboard title="Copy Paste URL" content={`https://pastefy.app/${paste.id}`} />
-                <Action.OpenInBrowser url={`https://pastefy.app/${paste.id}`} />
-                <Action.CreateSnippet title="Create Snippet from Paste" snippet={{
-                    name: paste.title,
-                    text: paste.content
-                }} />
-                <Action icon={Icon.Trash} title="Delete" onAction={onDelete} />
-            </ActionPanel>
-        }
-    />
-}
-
-
-export default function Command() {
-    const [searchText, setSearchText] = useState("");
-    const [loginError, setLoginError] = useState(false);
-    const [reload, setReload] = useState(false);
-
-    const { isLoading, data, pagination } = usePromise(
-    (searchText: string, reload: boolean) => async (options: { page: number }) => {
-        try {
-            const res = await axios.get(`https://pastefy.app/api/v2/user/pastes?page=${options.page+1}&search=${searchText}`, {
-                headers: {
-                    Authorization: `Bearer ${getPreferenceValues().apiKey}`
-                }
-            })
-
-            return { data: res.data, hasMore: res.data?.length > 0 };
-        } catch (e: any) {
-            if (e.response.status === 401) {
-                setLoginError(true)
-                throw new Error('Authentication failed');
-            }
-            throw new Error('Error while fetching pastes');
-        }
-    },
-    [searchText, reload]
-  );
-
-  const deletePaste = async (paste: any) => {
-    await axios.delete(`https://pastefy.app/api/v2/paste/${paste.id}`, {
-        headers: {
-            Authorization: `Bearer ${getPreferenceValues().apiKey}`
-        }
-    })
-    setReload(!reload)
+  } else {
+    markdown += createContentBlock(paste.title, paste.content);
   }
 
   return (
-    loginError ? <Detail actions={
-        <ActionPanel title="Pastefy Authentication">
-            <Action title="Add key" icon={Icon.AddPerson} onAction={openExtensionPreferences} />
+    <List.Item
+      icon={Icon.Code}
+      title={paste.title || "untitled"}
+      detail={<List.Item.Detail markdown={markdown} />}
+      actions={
+        <ActionPanel title="Paste Options">
+          <Action.CopyToClipboard title="Copy Paste Content" content={paste.content} />
+          <Action.Paste title="Paste Content" content={paste.content} />
+          <Action.CopyToClipboard title="Copy Paste URL" content={`https://pastefy.app/${paste.id}`} />
+          <Action.OpenInBrowser url={`https://pastefy.app/${paste.id}`} />
+          <Action.CreateSnippet
+            title="Create Snippet from Paste"
+            snippet={{
+              name: paste.title,
+              text: paste.content,
+            }}
+          />
+          <Action icon={Icon.Trash} title="Delete" onAction={onDelete} />
         </ActionPanel>
-    } markdown={`# Authentication failed
-You need to set a valid API-Key in the extensions preferences`} />
-    :   <List isShowingDetail isLoading={isLoading} onSearchTextChange={setSearchText} pagination={pagination}>
-            {data?.map((paste: any) => <PastePreview paste={paste} onDelete={() => deletePaste(paste)} />)}
-        </List>
+      }
+    />
+  );
+}
+
+export default function Command() {
+  const [searchText, setSearchText] = useState("");
+  const [loginError, setLoginError] = useState(false);
+
+  const { isLoading, data, pagination, revalidate } = usePromise(
+    (searchText: string) => async (options: { page: number }) => {
+      try {
+        const res = await axios.get(
+          `https://pastefy.app/api/v2/user/pastes?page=${options.page + 1}&search=${searchText}`,
+          {
+            headers: {
+              Authorization: `Bearer ${getPreferenceValues().apiKey}`,
+            },
+          },
+        );
+
+        return { data: res.data, hasMore: res.data?.length > 0 };
+      } catch (e) {
+        if (e instanceof AxiosError && e?.response?.status === 401) {
+          setLoginError(true);
+          throw new Error("Authentication failed");
+        }
+        throw new Error("Error while fetching pastes");
+      }
+    },
+    [searchText],
+  );
+
+  const deletePaste = async (paste: Paste) => {
+    await axios.delete(`https://pastefy.app/api/v2/paste/${paste.id}`, {
+      headers: {
+        Authorization: `Bearer ${getPreferenceValues().apiKey}`,
+      },
+    });
+    revalidate();
+  };
+
+  return loginError ? (
+    <Detail
+      actions={
+        <ActionPanel title="Pastefy Authentication">
+          <Action title="Add Key" icon={Icon.AddPerson} onAction={openExtensionPreferences} />
+        </ActionPanel>
+      }
+      markdown={`# Authentication failed
+You need to set a valid API-Key in the extensions preferences`}
+    />
+  ) : (
+    <List isShowingDetail isLoading={isLoading} onSearchTextChange={setSearchText} pagination={pagination}>
+      {data?.map((paste: Paste) => <PastePreview key={paste.id} paste={paste} onDelete={() => deletePaste(paste)} />)}
+    </List>
   );
 }
--- src/pastefy.tsx
+++ src/pastefy.tsx
@@ -14,47 +14,46 @@ type Values = {
   visibility: string;
 };
 
+// type PastefyFolder = { id: string; name: string; children: [] }
+type FolderListEntry = { id: string; path: string };
+
 export default function Command() {
+  const [password, setPassword] = useState("");
+  const [title, setTitle] = useState("");
 
-  const [password, setPassword] = useState('')
-  const [title, setTitle] = useState('')
-  const [reload, setReload] = useState(false)
+  const { isLoading, data: folders } = usePromise(async () => {
+    try {
+      const res = await axios.get("https://pastefy.app/api/v2/user/folders?hide_pastes=true", {
+        headers: {
+          Authorization: `Bearer ${getPreferenceValues().apiKey}`,
+        },
+      });
+      const folders: FolderListEntry[] = [];
 
-  const { isLoading, data: folders } = usePromise<any>(
-    () => async () => {
-      try {
-        const res = await axios.get(`https://pastefy.app/api/v2/user/folders?hide_pastes=true`, {
-          headers: {
-              Authorization: `Bearer ${getPreferenceValues().apiKey}`
-          }
-        })
-        const folders: { id: any; path: string; }[] = []
+      const addFolders = (folder: { id: string; name: string; children: [] }, prefix = "") => {
+        const folderName = prefix + folder.name;
+        folders.push({
+          id: folder.id,
+          path: folderName,
+        });
 
-        const addFolders = (folder: { id: any; name: string; children: [] }, prefix = '') => {
-          const folderName = prefix + folder.name
-          folders.push({
-            id: folder.id,
-            path: folderName,
-          })
-          
-          if (folder.children){
-            for (const subFolder of folder.children) {
-              addFolders(subFolder, `${folderName}/`)
-            }
+        if (folder.children) {
+          for (const subFolder of folder.children) {
+            addFolders(subFolder, `${folderName}/`);
           }
         }
+      };
 
-        for (const folder of res.data) {
-          addFolders(folder)
-        }
-
-        return { data: folders };
-      } catch {
-        // This fails if not logged in. Ignore it because you can use pastefy without being logged in.
-        return {data: null}
+      for (const folder of res.data) {
+        addFolders(folder);
       }
+
+      return folders;
+    } catch {
+      // This fails if not logged in. Ignore it because you can use pastefy without being logged in.
+      return null;
     }
-  );
+  });
 
   async function handleSubmit(values: Values) {
     showToast({ title: "Creating paste...", message: "Waiting for creation of paste" });
@@ -64,30 +63,32 @@ export default function Command() {
       encrypted: !!(password || values.encrypted),
       folder: values.folder || undefined,
       visibility: values.visibility,
-      expire_at: values.expire_at?.toISOString()?.slice(0, 19)?.replace('T', ' ') || undefined,
-    }
-    
-    let currentPassword = ''
+      expire_at: values.expire_at?.toISOString()?.slice(0, 19)?.replace("T", " ") || undefined,
+    };
+
+    let currentPassword = "";
 
     if (paste.encrypted) {
-      currentPassword = values.encrypted && !password ? Math.random().toString(36).substring(3) + Math.random().toString(36).substring(3) : password;
+      currentPassword =
+        values.encrypted && !password
+          ? Math.random().toString(36).substring(3) + Math.random().toString(36).substring(3)
+          : password;
 
-      paste.title = CryptoJS.AES.encrypt(paste.title, currentPassword).toString()
-      paste.content = CryptoJS.AES.encrypt(paste.content, currentPassword).toString()
+      paste.title = CryptoJS.AES.encrypt(paste.title, currentPassword).toString();
+      paste.content = CryptoJS.AES.encrypt(paste.content, currentPassword).toString();
     }
-
 
     const res = await axios.post(`https://pastefy.app/api/v2/paste`, paste, {
       headers: {
-          Authorization: `Bearer ${getPreferenceValues().apiKey}`
-      }
-    })
-    const pasteUrl = `https://pastefy.app/${res.data.paste.id}${currentPassword && !password ? '#'+currentPassword : ''}`
+        Authorization: `Bearer ${getPreferenceValues().apiKey}`,
+      },
+    });
+    const pasteUrl = `https://pastefy.app/${res.data.paste.id}${currentPassword && !password ? "#" + currentPassword : ""}`;
     await Clipboard.copy({
-      text: pasteUrl
+      text: pasteUrl,
     });
     showToast({ title: "Done!", message: "Paste has been saved to clipboard" });
-    open(pasteUrl)
+    open(pasteUrl);
   }
 
   return (
@@ -98,26 +99,38 @@ export default function Command() {
         </ActionPanel>
       }
     >
-      <Form.TextField onChange={setTitle} id="title" title="Title" placeholder="Title.txt (.md for markdown)" defaultValue="" />
-      <Form.TextArea id="content" enableMarkdown={title?.endsWith('.md')} title="Code input" placeholder="Enter multi-line text"  />
+      <Form.TextField
+        onChange={setTitle}
+        id="title"
+        title="Title"
+        placeholder="Title.txt (.md for markdown)"
+        defaultValue=""
+      />
+      <Form.TextArea
+        id="content"
+        enableMarkdown={title?.endsWith(".md")}
+        title="Code input"
+        placeholder="Enter multi-line text"
+      />
       <Form.Separator />
 
       <Form.Description text="Optional Settings" />
       <Form.Dropdown id="visibility" title="Visibility" defaultValue="UNLISTED">
         <Form.Dropdown.Item value="UNLISTED" title="Unlisted" />
         <Form.Dropdown.Item value="PUBLIC" title="Public" />
-        { folders === null ? null :<Form.Dropdown.Item value="PRIVATE" title="Private" /> }
+        {folders === null ? null : <Form.Dropdown.Item value="PRIVATE" title="Private" />}
       </Form.Dropdown>
       <Form.Checkbox id="encrypted" title="Checkbox" label="Client encrypted" storeValue />
       <Form.PasswordField onChange={setPassword} id="password" title="Password" />
-      { folders === null 
-        ? null 
-        : <Form.Dropdown isLoading={isLoading} id="folder" title="Folder">
-            <Form.Dropdown.Item value="" title="None" />
-            
-            {folders?.map((folder: any) => <Form.Dropdown.Item key={folder.id} value={folder.id} title={folder.path} />)}
-          </Form.Dropdown> 
-      }
+      {folders === null ? null : (
+        <Form.Dropdown isLoading={isLoading} id="folder" title="Folder">
+          <Form.Dropdown.Item value="" title="None" />
+
+          {folders?.map((folder: FolderListEntry) => (
+            <Form.Dropdown.Item key={folder.id} value={folder.id} title={folder.path} />
+          ))}
+        </Form.Dropdown>
+      )}
       <Form.DatePicker id="expire_at" title="Expire at" />
       {/* 
       TODO for the future if Pastefy will have support in it's frontend for it
